8a78aa26001d50e2326843850d7c7abd296aefba
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-03-17 18:00:56 -0300

    feat: implemented class track orders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,52 @@
-class TrackOrders:
-    # aqui deve expor a quantidade de estoque
-    def __len__(self):
-        pass
+from collections import Counter
 
-    def add_new_order(self, customer, order, day):
-        pass
+
+class TrackOrders:
+    def __init__(self) -> None:
+        self.orders = []
+
+    def __len__(self) -> int:
+        return len(self.orders)
+
+    def get_all_info(self, type):
+        return set([order[type] for order in self.orders])
+
+    def add_new_order(self, customer: str, order: str, day: str):
+        self.orders.append({"customer": customer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return Counter(
+            [
+                order["order"]
+                for order in self.orders
+                if order["customer"] == customer
+            ]
+        ).most_common(1)[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        all_orders_by_order = self.get_all_info("order")
+        customer_orders = [
+            order["order"]
+            for order in self.orders
+            if order["customer"] == customer
+        ]
+        return all_orders_by_order.difference(set(customer_orders))
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        all_orders_by_day = self.get_all_info("day")
+        customer_orders = [
+            order["day"]
+            for order in self.orders
+            if order["customer"] == customer
+        ]
+        return all_orders_by_day.difference(set(customer_orders))
 
     def get_busiest_day(self):
-        pass
+        return Counter([order["day"] for order in self.orders]).most_common(1)[
+            0
+        ][0]
 
     def get_least_busy_day(self):
-        pass
+        return Counter([order["day"] for order in self.orders]).most_common()[
+            -1
+        ][0]
