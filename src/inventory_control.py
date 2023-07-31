556d7dadf657bb3ddb57c1943af75428c43ee918
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-07-31 12:28:07 -0300

    feat: implemented get_available_dishes

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -22,12 +22,36 @@ class InventoryControl:
         self.orders = []
 
     def add_new_order(self, customer, order, day):
+        ingredients_in_use = self.get_quantities_to_buy()
+
+        for ingredient in self.INGREDIENTS[order]:
+            if (ingredients_in_use[ingredient] + 1) > self.MINIMUM_INVENTORY[
+                ingredient
+            ]:
+                return False
         self.orders.append(dict(customer=customer, order=order, day=day))
 
-    def get_quantities_to_buy(self):
+    def get_quantities_to_buy(self) -> dict[str, int]:
         ingredients_to_buy = {key: 0 for key in self.MINIMUM_INVENTORY.keys()}
         orders_qty = Counter([order["order"] for order in self.orders])
         for order, qty in orders_qty.items():
             for ingredient in self.INGREDIENTS[order]:
                 ingredients_to_buy[ingredient] += qty
         return ingredients_to_buy
+
+    def get_available_dishes(self) -> set[str]:
+        available_dishes = set()
+        ingredients_in_use = self.get_quantities_to_buy()
+
+        for order, ingredients in self.INGREDIENTS.items():
+            is_dish_valid = True
+
+            for ingredient in ingredients:
+                if (
+                    self.MINIMUM_INVENTORY[ingredient]
+                    - ingredients_in_use[ingredient]
+                ) == 0:
+                    is_dish_valid = False
+            if is_dish_valid:
+                available_dishes.add(order)
+        return available_dishes
